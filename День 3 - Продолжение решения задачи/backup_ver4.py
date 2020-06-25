31755a6b2638c55a52c930795f794c79f00858ed
iparinile <poselennov.il@ya.ru>
2020-06-25 16:11:39 +0500

    Работа над задачей

--- День 3 - Продолжение решения задачи/backup_ver4.py
+++ День 3 - Продолжение решения задачи/backup_ver4.py
@@ -5,7 +5,7 @@ import time
 source = ['C:\\Cods', 'C:\\Docs']
 
 # 2. Резервные копии должны храниться в основном каталоге резерва.
-target_dir = 'E:\\Backup'
+target_dir = 'E:\\диск1\\YandexDisk\\Backup'
 
 # 3. Файлы помещаются в zip-архив.
 # 4. Именем для zip-архива служит текущая дата и время.
@@ -13,12 +13,17 @@ today = target_dir + os.sep + time.strftime('%Y%m%d')
 # Текущее время служит именем zip-архива
 now = time.strftime('%H%M%S')
 
+# Запрашиваем комментарий пользователя для имени файла
+comment = input('Введите комментарий --> ')
+if len(comment) == 0:  # проверяем, введён ли комментарий
+    target = today + os.sep + now + '.zip'
+else:
+    target = today + os.sep + now + '_' + comment.replace(' ', '_') + '.zip'
+
 # Создаём каталог, если его ещё нет
 if not os.path.exists(today):
     os.mkdir(today)  # создание каталога
     print('Каталог успешно создан', today)
-# Имя zip-файла
-target = today + os.sep + now + '.zip'
 
 # 5. Используем команду "zip" для помещения файлов в zip-архив
 zip_command = "zip -qr {0} {1}".format(target, ' '.join(source))
